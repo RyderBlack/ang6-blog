9ba4bbede7b6de4a8716fc65df77aba891b7b45d
RyderBlack <ryhad.boughanmi@gmail.com>
2018-08-09 13:35:29 +0200

    data passed through nested comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Blogular';
 
+  /*
   lastUpdate = new Promise((resolve, reject) => {
     const date = new Date();
     setTimeout(
@@ -16,31 +17,32 @@ export class AppComponent {
       }, 2000
     );
   });
+*/
 
   posts = [
     {
       title: 'Veggie 1',
       content: 'Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.',
       loveIts: 54,
-      created_at: this.lastUpdate
+      created_at: 'nov'
     },
     {
       title: 'Veggie 2',
       content: 'Celery quandong swiss chard chicory earthnut pea potato. Salsify taro catsear garlic gram celery bitterleaf wattle seed collard greens nori. Grape wattle seed kombu beetroot horseradish carrot squash brussels sprout chard.',
       loveIts: 12,
-      created_at: this.lastUpdate
+      created_at: 'nov'
     },
     {
       title: 'Veggie 3',
       content: 'Beetroot water spinach okra water chestnut ricebean pea catsear courgette summer purslane. Water spinach arugula pea tatsoi aubergine spring onion bush tomato kale radicchio turnip chicory salsify pea sprouts fava bean. Dandelion zucchini burdock yarrow chickpea dandelion sorrel courgette turnip greens tigernut soybean radish artichoke wattle seed endive groundnut broccoli arugula.',
       loveIts: 32,
-      created_at: this.lastUpdate
+      created_at: 'nov'
     },
     {
       title: 'Veggie 4',
       content: 'Pea horseradish azuki bean lettuce avocado asparagus okra. Kohlrabi radish okra azuki bean corn fava bean mustard tigernut jícama green bean celtuce collard greens avocado quandong fennel gumbo black-eyed pea.',
       loveIts: -21,
-      created_at: this.lastUpdate
+      created_at: 'nov'
     }
   ];
 
