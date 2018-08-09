270af33d105bb7064dde38d2cf9096318fa56002
RyderBlack <ryhad.boughanmi@gmail.com>
2018-08-09 13:09:00 +0200

    add post list comp + post item comp

--- src/app/post-list-component/post-list-component.component.ts
+++ src/app/post-list-component/post-list-component.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListComponentComponent implements OnInit {
 
+  postTitle: string;
+  postContent: string;
+
   constructor() { }
 
   ngOnInit() {
