2e3353907d4874bd5c022b9f1a35a21f3d99d02c
RyderBlack <ryhad.boughanmi@gmail.com>
2018-08-15 14:46:32 +0200

    post service created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PostListComponentComponent } from './post-list-component/post-list-component.component';
 import { PostListItemComponentComponent } from './post-list-item-component/post-list-item-component.component';
+import { NewPostComponent } from './new-post/new-post.component';
+
+import { PostServiceService } from './post-service.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponentComponent,
-    PostListItemComponentComponent
+    PostListItemComponentComponent,
+    NewPostComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [PostServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/post-service.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PostServiceService {
+
+  private posts = [
+    {
+      title: 'Veggie 1',
+      content: 'Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.',
+      loveIts: 0,
+      created_at: new Date
+    },
+    {
+      title: 'Veggie 2',
+      content: 'Celery quandong swiss chard chicory earthnut pea potato. Salsify taro catsear garlic gram celery bitterleaf wattle seed collard greens nori. Grape wattle seed kombu beetroot horseradish carrot squash brussels sprout chard.',
+      loveIts: 12,
+      created_at: new Date
+    },
+    {
+      title: 'Veggie 3',
+      content: 'Beetroot water spinach okra water chestnut ricebean pea catsear courgette summer purslane. Water spinach arugula pea tatsoi aubergine spring onion bush tomato kale radicchio turnip chicory salsify pea sprouts fava bean. Dandelion zucchini burdock yarrow chickpea dandelion sorrel courgette turnip greens tigernut soybean radish artichoke wattle seed endive groundnut broccoli arugula.',
+      loveIts: -2,
+      created_at: new Date
+    },
+    {
+      title: 'Veggie 4',
+      content: 'Pea horseradish azuki bean lettuce avocado asparagus okra. Kohlrabi radish okra azuki bean corn fava bean mustard tigernut jícama green bean celtuce collard greens avocado quandong fennel gumbo black-eyed pea.',
+      loveIts: 21,
+      created_at: new Date
+    }
+  ];
+
+  constructor() { }
+}
